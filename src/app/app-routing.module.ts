c10e2bb5c66b61f22a8b22862bfbc7e7c1b58680
nika todua <127005879+nika-todua@users.noreply.github.com>
2024-07-26 11:27:24 +0400

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,10 +6,10 @@ import { ServicespageComponent } from './servicespage/servicespage.component';
 import { ContactpageComponent } from './contactpage/contactpage.component';
 
 const routes: Routes = [
-  { path: '', component: HomepageComponent },
-  { path: '/portfolio', component: PortfoliopageComponent },
-  { path: '/services', component: ServicespageComponent },
-  { path: '/contact', component: ContactpageComponent}
+  { path: '', redirectTo: '/hero', component: HomepageComponent },
+  { path: 'portfolio', component: PortfoliopageComponent },
+  { path: 'services', component: ServicespageComponent },
+  { path: 'contact', component: ContactpageComponent}
 ];
 
 @NgModule({
